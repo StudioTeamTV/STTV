31acc684fcf2805cd1c6a2a1079204dd9e91e0dc
StudioTeamTV <121429987+StudioTeamTV@users.noreply.github.com>
2022-12-27 19:41:36 +0100

    Update Soubor na nainstalováni .iso souboru.bat

--- ISO/Soubor na nainstalováni .iso souboru.bat
+++ ISO/Soubor na nainstalováni .iso souboru.bat
@@ -1,30 +1,31 @@
 @echo off
 color e
-title Instalator .iso
+title Instalator .iso Souboru
 net session
 cls
 C:
 cd "C:\Users\%username%\downloads"
 
-set /p aplikace=Zadejte nazev .iso, ktery chcete nainstalovat: 
+set /p iso=Zadejte nazev .iso, ktery chcete nainstalovat: 
 :start
 
 rem Win98
-if "%aplikace%"=="windows 98" set aplikace=Win98
-if "%aplikace%"=="Windows 98" set aplikace=Win98
-if "%aplikace%"=="Win98" set aplikace=Win98
-if "%aplikace%"=="win98" set aplikace=Win98
-if "%aplikace%"=="Win98" call :Instalace "https://winworldpc.com/download/417712c3-a418-c39a-11c3-a4e284a2c3a5/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2" "1"
+if "%iso%"=="windows 98" set iso=Win98
+if "%iso%"=="Windows 98" set iso=Win98
+if "%iso%"=="Win98" set iso=Win98
+if "%iso%"=="win98" set iso=Win98
+if "%iso%"=="Win98" call :Instalace "https://winworldpc.com/download/417712c3-a418-c39a-11c3-a4e284a2c3a5/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2" "1"
 
 echo Zadany soubor .iso neni na seznamu, budete ho muset nainstalovat rucne
 pause > nul
 cls
-set /p aplikace=Zadany soubor .iso nebyl spravne napiste ho znovu spravne: 
+set /p iso=Zadany soubor .iso nebyl spravne napiste ho znovu spravne: 
 goto start
 
 :Instalace
-echo Instaluji %aplikace%.....
+echo Instaluji %iso% a vypinam prikazovy radek (cmd.exe)
 start %~1
+call :exit
 if exist "%~2" del "%~2"
 :repeat
 timeout /t 0 > nul
@@ -33,3 +34,7 @@ if %~3 == 1 (start "%~2" "%~2") else (forfiles /m %~2 /c "cmd /c start @file @fi
 cls
 set /p aplikace=Zadejte nazev dalsiho .iso souboru, ktery chcete nainstalovat: 
 goto start
+
+:exit
+timeout /t 1
+exit
